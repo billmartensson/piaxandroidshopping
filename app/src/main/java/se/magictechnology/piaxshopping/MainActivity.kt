b4285e4672814a0581ced5580a5bac061719ff3c
Bill Mårtensson <bill@magictechnology.se>
2022-01-31 10:03:02 +0100

    piax

---
+++ app/src/main/java/se/magictechnology/piaxshopping/MainActivity.kt
@@ -0,0 +1,39 @@
+package se.magictechnology.piaxshopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("piaxandroid")
+
+        myRef.setValue("Hurra det funkar")
+
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d("piaxdebug", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("piaxdebug", "Failed to read value.", error.toException())
+            }
+        })
+    }
+}
